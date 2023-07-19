2a87abcff4d7af8aa5299ec63e4e76cd1f03bb70
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-19 12:45:15 -0300

    refactor: refactor db schema migrations and seeds

--- db/migrate/20230703183443_create_rentals.rb
+++ db/migrate/20230703183443_create_rentals.rb
@@ -1,9 +1,9 @@
 class CreateRentals < ActiveRecord::Migration[7.0]
   def change
-    create_table :rentals do |t|
-      t.references :movie, foreign_key: true
-      t.references :user, foreign_key: true
-      
+    create_table :rentals, primary_key: :id do |t|
+      t.references :movie, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
+
       t.timestamps
     end
 
