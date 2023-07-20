33405a9c59ce8d6f11e6e454eee1475bfcecb4c2
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-07-19 23:41:25 -0300

    refactor: refactor routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root "movies#index"
+  root 'movies#index'
 
-  resources :movies, only: %i[index] do
-    get :recommendations, on: :collection
-    get :user_rented_movies, on: :collection
-    get :rent, on: :member
+  resources :movies, only: [:index]
+  resources :users, only: [:show] do
+    get 'recommendations', to: 'users#recommendations', on: :member
+    get 'rented_movies', to: 'users#rented_movies', on: :member
   end
+  resources :rentals, only: [:create]
 end
